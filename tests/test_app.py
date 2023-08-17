d70d2d305528ebf9b368f9159ff8a59afdb2ba67
Kumani K <kumanikidd@gmail.com>
2023-08-17 15:07:59 +0100

    Update" " "
    git

--- tests/test_app.py
+++ tests/test_app.py
@@ -10,3 +10,12 @@ def test_get_albums(page, test_web_address, db_connection):
         "Title: 1989\nReleased: 2014",
         "Title: Voyage\nReleased: 2022"
     ])
+
+
+def test_single_album(page, test_web_address, db_connection):
+    db_connection.seed("seeds/music_library.sql")
+    page.goto(f"http://{test_web_address}/albums")
+    h1_tags = page.locator("h1")
+    p_tags = page.locator("p")
+    expect(p_tags).to_have_text("Release year: 2014\nArtist: Pixies")
+    expect(h1_tags).to_have_text("1989")
